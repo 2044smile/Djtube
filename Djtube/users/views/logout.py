1a9f352fe296ca877bbc14ac1f49c6ed16f0defd
ChangseokLee <2044smile@naver.com>
2020-02-01 14:04:54 +0900

    update LogoutView: add flash messages

--- Djtube/users/views/logout.py
+++ Djtube/users/views/logout.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import View
@@ -7,4 +8,9 @@ from django.contrib.auth import logout
 class LogoutView(View):
     def get(self, request, *args, **kwargs):
         logout(request)
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            '로그아웃이 성공적으로 되었습니다.',
+        )
         return redirect(reverse('login'))
