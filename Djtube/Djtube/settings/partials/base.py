68433f7c84e73213b308d0c55cbc9c60ea536748
2044smile <2044smile@naver.com>
2020-02-04 21:08:07 +0900

    update settings: add kakao social login set

--- Djtube/Djtube/settings/partials/base.py
+++ Djtube/Djtube/settings/partials/base.py
@@ -160,12 +160,17 @@ LOGOUT_SUCCESS_MESSAGE = '성공적으로 로그아웃 완료'
 
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.kakao.KakaoOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 ]
 
 #Facebook
 SOCIAL_AUTH_FACEBOOK_KEY = '2820024684762865'
 SOCIAL_AUTH_FACEBOOK_SECRET = 'b8dddc1b978f44520481da72c945fb59'
+
+#Kakao
+SOCIAL_AUTH_KAKAO_KEY = '891a168540fbb272a30fbb62111d1fa7'
+SOCIAL_AUTH_KAKAO_SECRET = 'a58d2e0e40de68f01667f169308f6e5e'
 
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
