be7a854e9b8c8ea247f2e7845d2e48150283bd63
2044smile <2044smile@naver.com>
2020-02-19 23:55:49 +0900

    refactor Post Model

--- Djtube/posts/urls.py
+++ Djtube/posts/urls.py
@@ -5,6 +5,8 @@ from posts.views import *
 
 app_name = 'posts'
 urlpatterns = [
+    path('', PostListView.as_view(), name='list'),
     path('new/', PostCreateView.as_view(), name='create'),
     path('confirm/', PostCreateConfirmView.as_view(), name='confirm'),
+    path('<int:pk>/', PostDetailView.as_view(), name='detail')
 ]
